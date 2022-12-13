b91cb8da0633a5a52def534d81c2429f94df8dc5
Manuel Guillermo Gil <manuelguillermogil@gmail.com>
2022-12-13 12:40:06 -0400

    feat: Show and hide navbar when the user scrolling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import LinkItem from '../LinkItem'
 import BurgerMenu  from './BurgerMenu'
 import BurgerButton  from './BurgerButton'
@@ -8,9 +8,26 @@ import '../../styles/navbar.scss'
 const Navbar = ({newPageSelected, currentPage}) => {
   const bluePointImage = 'https://static.wixstatic.com/media/c86aa8_b9d1a19b6fa148129b39180b7518a3ef~mv2.png/v1/fill/w_210,h_41,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/bluepoint2-Logo-Solid-DrkBlue.png';
   const [navbarOpen, setNavbarOpen] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(true);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 100) {
+      setShowNavbar(false)
+    } else {
+      setShowNavbar(true)
+    }
+  }
+
+  useEffect(() => {
+    console.log('aaaaa');
+    window.addEventListener('scroll', controlNavbar)
+    return () => {
+      window.removeEventListener('scroll', controlNavbar)
+    }
+  }, [])
 
   return (
-  <nav className="navbar">
+  <nav className={`navbar ${!showNavbar && 'hideNavbar'}`}>
     <div className="burger-section">
       <BurgerButton navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
       <BurgerMenu 
